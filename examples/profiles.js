6db91d3bbcc180220f4920099ecabf04e14217ce
Travis <travis@localhost.dev>
2017-09-22 15:18:14 +1000

    Remove node addresses in profiles.

--- examples/profiles.js
+++ examples/profiles.js
@@ -1,20 +1,4 @@
 var Profiles = {
-    Nodes: {
-        Mainnets: [
-            'http://seed1.neo.org:10332',
-            'http://seed2.neo.org:10332',
-            'http://seed3.neo.org:10332',
-            'http://seed4.neo.org:10332',
-            'http://seed5.neo.org:10332',
-        ],
-        Testnets: [
-            'http://seed1.neo.org:20332',
-            'http://seed2.neo.org:20332',
-            'http://seed3.neo.org:20332',
-            'http://seed4.neo.org:20332',
-            'http://seed5.neo.org:20332',
-        ],
-    },
     Assets: {
         NEO: 'c56f33fc6ecfcd0c225c4ab356fee59390af8560be0e930faebe74a6daff7c9b',
         GAS: '602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7',
