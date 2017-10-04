27f601d3be80980eaeb14029a57d8a2f5565591d
Travis <travis@localhost.dev>
2017-10-04 16:59:23 +1100

    Code refactor to 2-spaces indentation.

--- test/neo.blockchain.node.unit.test.js
+++ test/neo.blockchain.node.unit.test.js
@@ -12,13 +12,13 @@ const neoNode = neoBlockchain.nodeWithBlock(-1, 'latency', false);
 
 const mockHttpClient = new MockAdapter(axios, { delayResponse: 50 });
 const mockResponseData = {
-    getBlockCount: {
-        Success: {"jsonrpc":"2.0","id":0,"result":100000},
-    },
-    getBestBlockHash: {
-        Success: {"jsonrpc":"2.0","id":0,"result":"0xf760b3dd56a44e6b139d2a072d9f20ae503e13d01ef9f2385043b90f3a8ae876"},
-        Legacy: {"jsonrpc":"2.0","id":0,"result":"f760b3dd56a44e6b139d2a072d9f20ae503e13d01ef9f2385043b90f3a8ae876"}, // Deprecating (yet, not completely obsolete) hash syntax without leading '0x'.
-    },
+  getBlockCount: {
+    Success: {"jsonrpc":"2.0","id":0,"result":100000},
+  },
+  getBestBlockHash: {
+    Success: {"jsonrpc":"2.0","id":0,"result":"0xf760b3dd56a44e6b139d2a072d9f20ae503e13d01ef9f2385043b90f3a8ae876"},
+    Legacy: {"jsonrpc":"2.0","id":0,"result":"f760b3dd56a44e6b139d2a072d9f20ae503e13d01ef9f2385043b90f3a8ae876"}, // Deprecating (yet, not completely obsolete) hash syntax without leading '0x'.
+  },
 };
 
 // Experimental (and non functional)
@@ -29,68 +29,68 @@ const mockResponseData = {
 // mockHttpClient.onAny().passThrough();
 
 mockHttpClient.onPost().reply((config) => {
-    const dataObj = JSON.parse(config.data);
+  const dataObj = JSON.parse(config.data);
 
-    if (dataObj.method === 'getblockcount') {
-        return [200, mockResponseData.getBlockCount.Success];
-    } else if (dataObj.method === 'getbestblockhash') {
-        return [200, mockResponseData.getBestBlockHash.Success];
-    }
+  if (dataObj.method === 'getblockcount') {
+    return [200, mockResponseData.getBlockCount.Success];
+  } else if (dataObj.method === 'getbestblockhash') {
+    return [200, mockResponseData.getBestBlockHash.Success];
+  }
 
-    console.log('YOU SHOULDNT BE HERE!');
-    return [400, {}];
+  console.log('YOU SHOULDNT BE HERE!');
+  return [400, {}];
 });
 
 // Test cases
 
 describe('Unit test getBlockCount()', () => {
-    it('should have number as its response data type.', (resolve) => {
-        neoNode.getBlockCount()
-            .then((res) => {
-                const blockCount = res;
-                expect(typeof(blockCount)).to.equal('number');
-                resolve();
-            })
-            .catch((err) => {
-                resolve(err);
-            });
-    });
+  it('should have number as its response data type.', (resolve) => {
+    neoNode.getBlockCount()
+      .then((res) => {
+        const blockCount = res;
+        expect(typeof(blockCount)).to.equal('number');
+        resolve();
+      })
+      .catch((err) => {
+        resolve(err);
+      });
+  });
 
-    it('should have at least 1 block.', (resolve) => {
-        neoNode.getBlockCount()
-            .then((res) => {
-                const blockCount = res;
-                expect(blockCount).to.be.at.least(1);
-                resolve();
-            })
-            .catch((err) => {
-                resolve(err);
-            });
-    });
+  it('should have at least 1 block.', (resolve) => {
+    neoNode.getBlockCount()
+      .then((res) => {
+        const blockCount = res;
+        expect(blockCount).to.be.at.least(1);
+        resolve();
+      })
+      .catch((err) => {
+        resolve(err);
+      });
+  });
 });
 
 describe('Unit test getBestBlockHash()', () => {
-    it('should have string as its response data type.', (resolve) => {
-        neoNode.getBestBlockHash()
-            .then((res) => {
-                const hash = res;
-                expect(typeof(hash)).to.equal('string');
-                resolve();
-            })
-            .catch((err) => {
-                resolve(err);
-            });
-    });
+  it('should have string as its response data type.', (resolve) => {
+    neoNode.getBestBlockHash()
+      .then((res) => {
+        const hash = res;
+        expect(typeof(hash)).to.equal('string');
+        resolve();
+      })
+      .catch((err) => {
+        resolve(err);
+      });
+  });
 
-    it("should be '0x' follow by 64 hex characters in lower-case.", (resolve) => {
-        neoNode.getBestBlockHash()
-            .then((res) => {
-                const hash = res;
-                expect(hash).to.match(/^(0x)[a-f0-9]{64}$/);
-                resolve();
-            })
-            .catch((err) => {
-                resolve(err);
-            });
-    });
+  it("should be '0x' follow by 64 hex characters in lower-case.", (resolve) => {
+    neoNode.getBestBlockHash()
+      .then((res) => {
+        const hash = res;
+        expect(hash).to.match(/^(0x)[a-f0-9]{64}$/);
+        resolve();
+      })
+      .catch((err) => {
+        resolve(err);
+      });
+  });
 });
