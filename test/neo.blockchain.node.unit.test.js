5f11d0c197ddb477c8a560940988034b6a3ac2a4
Travis <travis@localhost.dev>
2017-10-04 13:51:25 +1100

    Incremental structural updates on unit test setup.

--- test/neo.blockchain.node.unit.test.js
+++ test/neo.blockchain.node.unit.test.js
@@ -6,10 +6,6 @@ const MockAdapter = require('axios-mock-adapter');
 // Mock Adapter
 
 const mockHttpClient = new MockAdapter(axios, { delayResponse: 50 });
-const mockRequestData = {
-    getBlockCount: '{"jsonrpc":"2.0","method":"getblockcount","params":[],"id":0}',
-    getBestBlockHash: '{"jsonrpc":"2.0","method":"getbestblockhash","params":[],"id":0}',
-};
 const mockResponseData = {
     getBlockCount: {
         Success: {"jsonrpc":"2.0","id":0,"result":100000},
@@ -20,12 +16,18 @@ const mockResponseData = {
     },
 };
 
-mockHttpClient.onAny().reply((config) => {
-    if (config.data == mockRequestData.getBlockCount) {
-        return [200, mockResponseData.getBlockCount];
-    } else if (config.data == mockRequestData.getBestBlockHash) {
-        // console.log('[BOOM] onAny mock. confing:', config);
-        // return [503, {}];
+// Experimental (and non functional)
+// mockHttpClient.onPost('/', '{"jsonrpc":"2.0","method":"getbestblockhash","params":[],"id":0}').reply(505, {}); // Doesn't work
+// mockHttpClient.onPost('/', {body: '{"jsonrpc":"2.0","method":"getbestblockhash","params":[],"id":0}'}).reply(505, {}); // Doesn't work
+// mockHttpClient.onPost('/', {data: '{"jsonrpc":"2.0","method":"getbestblockhash","params":[],"id":0}'}).reply(505, {}); // Doesn't work
+// mockHttpClient.onPost('/', {body: {data: '{"jsonrpc":"2.0","method":"getbestblockhash","params":[],"id":0}'}}).reply(505, {}); // Doesn't work
+// mockHttpClient.onAny().passThrough();
+
+mockHttpClient.onPost().reply((config) => {
+    const dataObj = JSON.parse(config.data);
+    if (dataObj.method === 'getblockcount') {
+        return [200, mockResponseData.getBlockCount.Success];
+    } else if (dataObj.method === 'getbestblockhash') {
         return [200, mockResponseData.getBestBlockHash.Success];
     }
 
@@ -35,18 +37,47 @@ mockHttpClient.onAny().reply((config) => {
 
 // Bootstrapping
 
-const mode = 'light';
-const neoBlockchain = new neo(mode, 'testnet');
+const neoBlockchain = new neo('light', 'testnet');
 const neoNode = neoBlockchain.nodeWithBlock(-1, 'latency', false);
 
 // Test cases
 
-describe(`Unit Test getBestBlockHash()`, () => {
+describe('Unit test getBlockCount()', () => {
+    it('should have number as its response data type.', (resolve) => {
+        neoNode.getBlockCount()
+            .then((res) => {
+                console.log('getBlockCount. res:', res);
+                const blockCount = res;
+                expect(typeof(blockCount)).to.equal('number');
+                resolve();
+            });
+    });
+
+    it('should have at least 1 block.', (resolve) => {
+        neoNode.getBlockCount()
+            .then((res) => {
+                const blockCount = res;
+                expect(blockCount).to.be.at.least(1);
+                resolve();
+            });
+    });
+});
+
+describe('Unit test getBestBlockHash()', () => {
     it('should have string as its response data type.', (resolve) => {
         neoNode.getBestBlockHash()
             .then((res) => {
-                let hash = res;
+                const hash = res;
                 expect(typeof(hash)).to.equal('string');
+                resolve();
+            });
+    });
+
+    it("should be '0x' follow by 64 hex characters in lower-case.", (resolve) => {
+        neoNode.getBestBlockHash()
+            .then((res) => {
+                const hash = res;
+                expect(hash).to.match(/^(0x)[a-z0-9]{64}$/);
                 resolve();
             });
     });
