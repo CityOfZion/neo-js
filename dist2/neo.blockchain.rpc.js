d42ba11de7c380f741ceed942e7381c4bc94e059
Travis <travis@localhost.dev>
2017-10-24 08:48:31 +1100

    Change RPC class that it only takes node URL oppose to additional logic

--- dist2/neo.blockchain.rpc.js
+++ dist2/neo.blockchain.rpc.js
@@ -3,34 +3,42 @@
  * @param {String} network can be either 'mainnet' or 'testnet'
  * @param {Object} options 
  */
-const Rpc = function (network, options = {}) {
+const Rpc = function (nodeUrl, options = {}) {
   // Properties and default values
-  this.network = network
-  this.enum = options.enum || require('./neo.blockchain.enum')
-  this._ = options._ || require('lodash')
+  this.nodeUrl = nodeUrl
   this.axios = options.axios || require('axios')
-  this.node = undefined
-
-  // Bootstrap
-  this.setDefaultNode()
 }
 
 Rpc.prototype = {
-  setDefaultNode: function () {
-    this.node = this.enum.nodes[this.network][0]
-  },
-
-  getCurrentNode: function () {
-    return this.node
-  },
-
-  getCurrentNodeUrl: function () {
-    return this.node.url + ':' + this.node.port
-  },
-
+  /**
+   * Gets the best block hash on the node
+   * @example
+   * node.getBestBlockHash()
+   * return 0x051b5bf812db0536e488670b26abf3a45a5e1a400595031cf9a57416bea0b973
+   * @returns {Promise.<Object>}
+   */
   getBestBlockHash: function () {
     return this._call({
       method: 'getbestblockhash',
+      params: [],
+      id: 0
+    })
+  },
+
+  /**
+   * Invokes the getblockcount rpc request to return the block height.  This
+   * method will request the block height from the fastest active node with failover if a
+   * node is not provided.  This method will update the blockHeight attribute
+   * on the node it is run on.
+   *
+   * @example
+   * node.getBlockCount()
+   * return 1000000
+   * @returns {Promise.<number>} A promise returning the block count.
+   */
+  getBlockCount: function() {
+    return this._call({
+      method: 'getblockcount',
       params: [],
       id: 0
     })
@@ -40,7 +48,7 @@ Rpc.prototype = {
     return new Promise((resolve, reject) => {
       this.axios({
         method: 'post',
-        url: this.getCurrentNodeUrl(),
+        url: this.nodeUrl,
         data: {
           jsonrpc: '2.0',
           method: payload.method,
--- examples/rpc.js
+++ examples/rpc.js
@@ -4,23 +4,30 @@ const neoRpc = require('../dist2/neo.blockchain.rpc')
 
 // -- Bootstrap
 
-let client = new neoRpc('testnet')
+// let client = new neoRpc('testnet')
+let client = new neoRpc('http://seed1.neo.org:20332')
 // console.log('client:', client)
 
 //-- Use Cases
 
-async function basic () {
-  console.log('basic:')
-  console.log('getCurrentNode:', client.getCurrentNode())
-  console.log('getCurrentNodeUrl:', client.getCurrentNodeUrl())
+async function nodeProfiles () {
+  console.log('== nodeProfiles: ==')
+  // console.log('getCurrentNode:', client.getCurrentNode())
+  // console.log('getCurrentNodeUrl:', client.getCurrentNodeUrl())
   console.log('bestBlockHash:', await client.getBestBlockHash())
+  console.log('getBlockCount:', await client.getBlockCount())
 }
 
 // -- Chain of command
 
 async function actionAsync () {
-  // Examples
-  await basic()
+  /**
+   * NOTES:
+   * - RPC should have no concerns about available nodes and the need to switch between nodes.
+   *   The RPC constructor should just take the url path, that's all it should cares.
+   * - Want to change the node URL? either reinstantiate, or directly change the URL
+   */
+  await nodeProfiles()
   process.exit()
 }
 
