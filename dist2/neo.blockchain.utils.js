46ae4057c12af7f5f99b12ae2ca70ffa256583b7
Travis <travis@localhost.dev>
2017-10-27 11:50:25 +1100

    Add utils library

---
+++ dist2/neo.blockchain.utils.js
@@ -0,0 +1,24 @@
+/**
+ * Neo utils library.
+ */
+module.exports = {
+  /**
+   * TBA
+   */
+  normaliseHash: function(hash, withHexPrefix = true) {
+    if(withHexPrefix) {
+      if (hash.length === 64) { // TODO: better validation method
+        return '0x' + hash
+      } else {
+        return hash
+      }
+    } else {
+      if (hash.length > 64) { // TODO: better validation method
+        return hash.slice(2)
+      } else {
+        return hash
+      }
+    }
+  },
+
+}
