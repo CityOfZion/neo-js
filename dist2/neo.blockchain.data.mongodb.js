26b92feb6c86bbe8c967bf11e981c476d8b4af0d
Travis <travis@localhost.dev>
2017-10-27 12:03:26 +1100

    Further scaffold mongodb dataaccess

--- dist2/neo.blockchain.data.mongodb.js
+++ dist2/neo.blockchain.data.mongodb.js
@@ -1,11 +1,28 @@
+const mongoose = require('mongoose')
+const _ = require('lodash')
+
+/**
+ * MongoDB Data Access
+ * @class
+ * @public
+ * @param {Object} options 
+ */
 const MongoDA = function (options = {}) {
   // Properties and default values
-
+  this.options = _.assign({}, MongoDA.Defaults, options)
+  
   // Bootstrap
+  
+}
+
+/**
+ * Default options for RPC client.
+ * @public
+ */
+MongoDA.Defaults = {
 }
 
 MongoDA.prototype = {
-
 }
 
 module.exports = MongoDA
