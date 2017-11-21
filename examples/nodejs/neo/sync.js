4ed26716df9ff6861ce8c1e36cb60ad5d0133f5e
Travis <travis@localhost.dev>
2017-11-21 23:49:42 +1100

    Further draft implementation

--- examples/nodejs/neo/sync.js
+++ examples/nodejs/neo/sync.js
@@ -12,6 +12,7 @@
 // -- Bootstrap
 
 const Neo = require('../../../dist/neo.blockchain.neo').neo
+const mongoose = require('mongoose')
 
 // -- Chain of command
 
@@ -21,10 +22,27 @@ async function main () {
   // Instantiate a 'full' mode Neo blockchain against 'testnet' network
   const neoBlockchain = new Neo('full', 'testnet')
 
+  // Explicit promise class assignment (needed to avoid warning message)
+  mongoose.Promise = global.Promise
+  // Establish MongoDB connection with hardcoded connection string
+  await mongoose.connect('mongodb://localhost/neo', { useMongoClient: true })
+
+  // Drop all existing collections in database in order to start fresh
+  console.log('existing collections:')
+  const collectionsInfo = await mongoose.connection.db.listCollections().toArray()
+  collectionsInfo.forEach(async (info, index) => {
+    console.log(`> dropping [${index}] [${info.name}] collection...`)
+    await mongoose.connection.db.dropCollection(info.name)
+  })
+
   // Allow it to sync for 30 seconds
+  console.log('Start syncing...')
   neoBlockchain.sync.start()
   await sleep(30000)
   neoBlockchain.sync.stop()
+  console.log('Stop syncing...')
+
+  // TODO: browse collections, analysed what's been stored from blockchain sync
 
   console.log('== END ==')
 
