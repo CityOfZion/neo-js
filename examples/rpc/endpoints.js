01ca5f1c485d2470cf19c1d0f2723748daa1a6aa
Travis <rockacola@gmail.com>
2018-01-29 23:54:51 +1100

    Update RPC endpoint example usage to provide better diagnostic data

--- examples/rpc/endpoints.js
+++ examples/rpc/endpoints.js
@@ -20,19 +20,60 @@ async function main () {
    */
   // const node = new Node({ network: 'testnet' })
   // const node = new Node({ network: 'mainnet' })
-  const node = new Node({ network: {
+
+  console.log('TestNet:')
+  const testnetNode = new Node({ network: {
     endpoints: [
-      { domain: 'http://seed2.neo.org', port: 10332 },
-      { domain: 'http://seed1.cityofzion.io', port: 8080 }
+      { domain: 'http://seed1.neo.org', port: 20332 },
+      { domain: 'http://seed2.neo.org', port: 20332 },
+      { domain: 'http://seed3.neo.org', port: 20332 },
+      { domain: 'http://seed4.neo.org', port: 20332 },
+      { domain: 'http://seed5.neo.org', port: 20332 },
+      { domain: 'http://test1.cityofzion.io', port: 8880 },
+      { domain: 'http://test2.cityofzion.io', port: 8880 },
+      { domain: 'http://test3.cityofzion.io', port: 8880 },
+      { domain: 'http://test4.cityofzion.io', port: 8880 },
+      { domain: 'http://test5.cityofzion.io', port: 8880 },
     ]
   }})
 
   // Test connection by verify block count of every RPC endpoints
-  await Promise.all(node.mesh.nodes.map(async (node) => {
-    logger.info(`> ${node.domain}:${node.port} block count:`, await node.rpc.getBlockCount())
+  await Promise.all(testnetNode.mesh.nodes.map(async (node) => {
+    try {
+      const blockCount = await node.rpc.getBlockCount()
+      logger.info(`> ${node.domain}:${node.port} block count:`, blockCount)
+    } catch (ex) {
+      logger.info(`> ${node.domain}:${node.port} error:`, ex.message)
+    }
   }))
 
-  logger.info('== END ==')
+  console.log('MainNet:')
+  const mainnetNode = new Node({ network: {
+    endpoints: [
+      { domain: 'http://seed1.neo.org', port: 10332 },
+      { domain: 'http://seed2.neo.org', port: 10332 },
+      { domain: 'http://seed3.neo.org', port: 10332 },
+      { domain: 'http://seed4.neo.org', port: 10332 },
+      { domain: 'http://seed5.neo.org', port: 10332 },
+      { domain: 'http://seed1.cityofzion.io', port: 8080 },
+      { domain: 'http://seed2.cityofzion.io', port: 8080 },
+      { domain: 'http://seed3.cityofzion.io', port: 8080 },
+      { domain: 'http://seed4.cityofzion.io', port: 8080 },
+      { domain: 'http://seed5.cityofzion.io', port: 8080 }
+    ]
+  }})
+
+  // Test connection by verify block count of every RPC endpoints
+  await Promise.all(mainnetNode.mesh.nodes.map(async (node) => {
+    try {
+      const blockCount = await node.rpc.getBlockCount()
+      logger.info(`> ${node.domain}:${node.port} block count:`, blockCount)
+    } catch (ex) {
+      logger.info(`> ${node.domain}:${node.port} error:`, ex.message)
+    }
+  }))
+
+  console.log('== END ==')
 
   // neoBlockchain process in the background. Explicit exit call is used.
   process.exit()
