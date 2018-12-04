30501327c4f24c31fecbf32801dcd7dd43f87af7
Travis <rockacola@gmail.com>
2018-12-04 23:03:37 +1100

    Correct block schema

--- src/storages/mongodb/schemas.ts
+++ src/storages/mongodb/schemas.ts
@@ -2,13 +2,28 @@ import { Schema } from 'mongoose'
 
 export const BlockSchema = new Schema(
   {
-    height: { type: 'Number', unique: true, required: true, dropDups: true },
-    time: Number,
-    size: Number,
-    generationTime: Number,
-    transactionCount: Number,
+    height: Number,
     createdBy: String,
-    apiLevel: Number,
+    source: String,
+    userAgent: String,
+    payload: {
+      hash: String,
+      size: Number,
+      version: Number,
+      previousblockhash: String,
+      merkleroot: String,
+      time: Number,
+      index: { type: 'Number', required: true },
+      nonce: String,
+      nextconsensus: String,
+      script: {
+        invocation: String,
+        verification: String,
+      },
+      tx: [],
+      confirmations: Number,
+      nextblockhash: String,
+    },
   },
   { timestamps: true }
 )
